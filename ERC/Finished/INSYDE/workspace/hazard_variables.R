b8989d99682c2568704ea0401f36f7d337cd0363
MarkusKonk <m_konk01@uni-muenster.de>
2020-07-06 13:31:10 +0200

    update insyde and bindings.json

---
+++ ERC/Finished/INSYDE/workspace/hazard_variables.R
@@ -0,0 +1,7 @@
+# Hazard variables
+
+he <- seq(0, 5, 0.01)  # water depth (m)
+velocity <- 0.5   # velocity (m/s)
+sediment_concentration <- 0.05  # sediment concentration (-)
+duration <- 24    # flood duration (h)
+water_quality <- 1     # water quality (presence of pollutants) 1=yes 0=no
